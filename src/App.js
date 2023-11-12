a037d43e7d97488f5a37a3173835d7b14d1baf26
mars-96 <mars73348@gmail.com>
2023-11-12 21:26:34 +0500

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+// function App(props) {
+//   const colorStyle = {
+//     color:props.color,
+//     fontSize:props.size
+//   }
+//   return (
+//     <div>
+//       <div style={colorStyle}>
+//       Hello World!
+//       </div>
+//     </div>
+//   );
+// }
 
-function App() {
+// export default App;
+
+// import React, {useState} from 'react';
+
+// const App = (props)=> {
+//   const [counter, setCounter] = useState(0);
+
+//   let incrementCounter = ()=> {
+//     setCounter(counter+1);
+//   }
+//   let colorStyle = {color:props['color'],fontSize:props['size']}
+//     return (
+//       <div style={colorStyle}>
+//         React Component
+//         <br /><br />
+//         <button onClick={incrementCounter}>Click Me!</button>
+//         <br /><br />
+//         {counter}
+//       </div>
+//     );
+//   }
+import axios from 'axios';
+import React, { useState, useEffect } from "react";
+
+const App = (props) => {
+  //define the state and the setter
+  const [APIlist, setAPIlist] = useState();
+
+  //On load invoke method to genrate list
+  useEffect(() => {
+    let url = "https://api.publicapis.org/entries?category=Animals";
+    axios({
+      method: 'get',
+      url: url,
+      responseType: 'json'
+    }).then(resp => {
+      let listOfEntries = resp.data.entries;
+      let listOfEntriesAsArray = Object.entries(listOfEntries);
+      let i = 1;
+      let entryDetails = listOfEntriesAsArray.map((entryDetail) => {
+        return (
+          <tr key={i++}>
+            <td key={i++} style={{ color: "red", border: "1px solid black" }}>					{entryDetail[1]["API"]} </td>
+            <td key={i++} style={{ color: "red", border: "1px solid black" }}> 					{entryDetail[1]["Link"]} </td>
+          </tr>)
+      })
+      setAPIlist(entryDetails);
+    })
+      .catch(err => {
+        console.log(err.toString())
+      });
+  }, []);
+
+  const colorStyle = { color: props['color'], fontSize: props['size'] }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>APIs List</h2>
+      <br />
+
+      <div style={colorStyle}><ul>{APIlist}</ul></div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App color="green" size="20px" />
   </React.StrictMode>
 );
 
@@ -15,3 +15,11 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// import React from 'react';
+// import App from './App';
+// import { createRoot } from 'react-dom/client';
+
+// // This is how it it is done from React 18 onwards
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<App />);
